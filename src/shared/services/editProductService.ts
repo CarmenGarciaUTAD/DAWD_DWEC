464ed9afd31b93873389e0462bd9f46ac7b33cdd
equipoCarmen_2024 <carmen.garcia2@live.u-tad.comBienvenido a U-tad Carmen Garcia Lopez! Te informamos que hemos dado de alta tu usuario en nuestros sistemas. Tu usuario U-Tad es el siguiente: carmen.garcia2@live.u-tad.com Bienvenido a U-tad Carmen Garcia Lopez! Te informamos que hemos dado de alta tu usuario en nuestros sistemas. Tu usuario U-Tad es el siguiente: carmen.garcia2@live.u-tad.com>
2025-03-11 12:54:43 +0100

    quinto commit editProduct

---
+++ src/shared/services/editProductService.ts
@@ -0,0 +1,24 @@
+import { productSchema } from "../schemas/product.schema";
+
+export async function editproductService(product: productSchema, id: number) {
+    const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/rest/v1/${id}`,
+        {
+            method: "PUT",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Bearer ${localStorage.getItem("token")}`
+            },
+            body: JSON.stringify(product),
+        }
+    );
+
+    if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message || "Error al actualizar product")
+    }
+
+    const data = await response.json()
+
+    return data;
+}
